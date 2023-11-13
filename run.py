befd8c90ba446a91748d1c7359568d62b419955b
irebas <igor.rebas@gmail.com>
2023-11-13 14:58:52 +0100

    new summary

--- run.py
+++ run.py
@@ -6,3 +6,5 @@ main()
 # zmienić clients discount_2 zeby to sie wyliczalo na podstawie zakladki params2
 # filtrowane sa tylko te wiersze gdzie klinet jest w tabeli clients a indeks z tabeli pl
 # tabela klient vs segment i wyliczanie discount_3
+
+# TU BEDA ZMIANY
