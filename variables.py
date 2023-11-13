45cf5b285a820ee9aa3cabbcb008ae8d848192c6
irebas <igor.rebas@gmail.com>
2023-11-13 13:46:29 +0100

    sheets remove

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
 DB_NAME = 'rawlplug.db'
 RESULTS_PATH = 'inputs/Tabela_1.csv'
 PARAMS_PATH = 'inputs/params.xlsx'
-SHEETS = ['params1', 'params2', 'volume', 'volume2', 'volume3', 'prices_logic', 'elasticity', 'clients']
+SHEETS = ['params1', 'params2', 'volume2', 'volume3', 'prices_logic', 'elasticity', 'clients']
