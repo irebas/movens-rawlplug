9f24027a9642f6f215643a0fdefe39a99968e720
irebas <igor.rebas@gmail.com>
2023-11-20 00:09:23 +0100

    final commit

--- queries/v_summary.sql
+++ queries/v_summary.sql
@@ -1,7 +1,11 @@
-CREATE VIEW summary AS
+CREATE VIEW summary_det AS
 SELECT
+	klient,
 	product_group,
+	indeks,
 	SUM(w_PLN) AS wartosc_sprzedazy_status_quo,
+	SUM(w_ilosc) AS wolumen,
+	SUM(elast_volume) AS wolument_elast,
 	SUM(masa_marzy_status_quo) AS masa_marzy_status_quo,
 	SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki) AS wartosc_sprzedazy_w_cenach_kat_z_logiki,
 	SUM(wartosc_sprzedazy_w_cenach_z_logiki) AS wartosc_sprzedazy_w_cenach_z_logiki,
@@ -15,5 +19,5 @@ SELECT
 	SUM(masa_marzy_elast) / SUM(masa_marzy_status_quo) - 1 AS zmiana_masy_marzy_vs_status_quo_2,
 	SUM(wartosc_sprzedazy_w_cenach_z_logiki) / SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki) - 1 AS sredni_poziom_rabatowania
 FROM data_all
-WHERE product_group IS NOT NULL AND cena_transakcyjna NOT IN (0.01, 1)
-GROUP BY 1
+WHERE product_group IS NOT NULL -- AND cena_transakcyjna NOT IN (0.01, 1)
+GROUP BY 1,2,3
--- queries/v_summary_3.sql
+++ queries/v_summary_3.sql
@@ -3,10 +3,12 @@ SELECT
 	klient,
 	product_group,
 	indeks,
-	SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki * discount_1) AS discount_1,
-	SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki * discount_2) AS discount_2,
-	SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki * discount_3) AS discount_3,
-	SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki * discount_4) AS discount_4,
-	SUM(wartosc_sprzedazy_w_cenach_kat_z_logiki * discount_5) AS discount_5
+	SUM(wartosc_sprzedazy_w_cenach_kat_elast * discount_1) AS discount_1,
+	SUM(wartosc_sprzedazy_w_cenach_kat_elast * discount_2) AS discount_2,
+	SUM(wartosc_sprzedazy_w_cenach_kat_elast * discount_3) AS discount_3,
+	SUM(wartosc_sprzedazy_w_cenach_kat_elast * discount_4) AS discount_4,
+	SUM(wartosc_sprzedazy_w_cenach_kat_elast * discount_5) AS discount_5,
+	SUM(wartosc_sprzedazy_w_cenach_logika_elast) AS wartosc_sprzedazy_w_cenach_z_logiki_elast,
+	SUM(wartosc_sprzedazy_w_cenach_kat_elast) AS wartosc_sprzedazy_w_cenach_kat_elast
 FROM data_all
 GROUP BY 1,2,3
